eba45edb36f75eed5f17b964a83a620ff075808f
kainanpinheiro <kainan.silva@aluno.faculdadeimpacta.com.br>
2020-11-26 22:24:29 -0300

    efetuar venda

--- web/frontend-styled-components/src/pages/Vendas/index.tsx
+++ web/frontend-styled-components/src/pages/Vendas/index.tsx
@@ -1,147 +1,153 @@
-import React, { FormEvent, useState } from 'react'
-import { FaPlus } from "react-icons/fa"
-import api from "../../services/api"
+import React, { FormEvent, useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
+import api from '../../services/api';
+import { Header } from '../../components/Header';
 
-import { Header } from "../../components/Header"
+import './index.css';
+import { formatPrice } from '../../utils/format';
 
-import './index.css'
-import { formatPrice } from '../../utils/format'
+interface produtos {
+  nome: string;
+  qtd: string;
+  preco: string;
+}
 
-function Vendas() { 
-    const [produtos, setProdutos] = useState([
-        {nome: '', qtd: 0, preco: 0}
-    ])
-    // const [qtd, setQtd] = useState("0")
-    // const [preco, setPreco] = useState("0")
-    const [token, setToken] = useState("")
+function Vendas() {
+  const [produtos, setProdutos] = useState<produtos[]>([]);
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [qtd, setQtd] = useState('0');
+  const [preco, setPreco] = useState('0');
+  const [token, setToken] = useState('');
 
-    function addNewProduct() {
-        setProdutos([
-            ...produtos,
-            {nome: '', qtd: 0, preco: 0}
-        ])
+  function addNewProduct() {
+    if (!nomeProduto || !qtd || !preco) alert('Preencha os campos!');
+    else {
+      setProdutos([...produtos, { nome: nomeProduto, qtd, preco }]);
+      setNomeProduto('');
+      setQtd('');
+      setPreco('');
     }
+  }
 
-    function setProductItemValue(position: number, field: string, value:string) {
-        const updateProductItems = produtos.map((produto, index) => {
-            if (index === position) {
-                return { ...produtos, [field]: value }
-            }
-            return produto
-        });
-        setProdutos(updateProductItems);
-    }
+  function deleteProduct(nomeProduct: string) {
+    const newProducts = produtos.filter((p) => p.nome !== nomeProduct);
 
-    function handleButton(event: FormEvent) {
-        event.preventDefault()
+    setProdutos(newProducts);
+  }
 
-        api.post("/auth", {username: "teste", password: "teste"}).then((response) => {
-            setToken(response.data.access_token)
-        })
+  function handleButton(event: FormEvent) {
+    event.preventDefault();
 
-        api.post("/insert/produto", {produtos}, {headers: {"Authorization": `JWT ${token}`}}).then((responde) => {
-            alert("Produto Cadastrado com sucesso !!!")
-        })
-        .catch(() => {
-            alert("Esse Produto não pode ser cadastrado !!!")
-        })
-    }
-    
-  
-    return (
-        <>
-        <Header></Header>
-       <div className="container">
+    api
+      .post('/auth', { username: 'teste', password: 'teste' })
+      .then((response) => {
+        setToken(response.data.access_token);
+      });
+
+    api
+      .post(
+        '/insert/produto',
+        { produtos },
+        { headers: { Authorization: `JWT ${token}` } },
+      )
+      .then((responde) => {
+        alert('Produto Cadastrado com sucesso !!!');
+      })
+      .catch(() => {
+        alert('Esse Produto não pode ser cadastrado !!!');
+      });
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <div className="container">
         <form onSubmit={handleButton}>
-            <h1>💰 Efetuar Venda 💰</h1>
+          <h1>💰 Efetuar Venda 💰</h1>
 
-            <div className="input-container">
-                <div className="input-block">
-                    <label>Produto</label>
-                    <input type="text" required value={produto}  onChange={(e) => setProduto(e.target.value) }/>
-                </div>
-
-                <div className="input-block">
-                    <label>Quantidade</label>
-                    <input type="number" required value={qtd} onChange={(e) => setQtd(e.target.value) }/>
-                </div>
-
-                <div className="input-block">
-                    <label>Preço</label>
-                    <input type="number" required value={preco} onChange={(e) => setPreco(e.target.value) }/>
-                </div>
-
-                <div className="add-button">
-                   <button onClick={addNewProduct}><FaPlus size={25} color="#000"/> Adicionar</button>
-                </div>
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Quantidade</th>
-                            <th>Preço</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <span>Pizza de Frango</span>
-                            </td>
-
-                            <td>
-                                <span>2</span>
-                            </td>
-
-                            <td>
-                                <span>{formatPrice(30)}</span>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <span>Pizza de Mussarela</span>
-                            </td>
-
-                            <td>
-                                <span>2</span>
-                            </td>
-
-                            <td>
-                                <span>{formatPrice(36)}</span>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <span>Coca-Cola</span>
-                            </td>
-
-                            <td>
-                                <span>2</span>
-                            </td>
-
-                            <td>
-                                <span>{formatPrice(20)}</span>
-                            </td>
-                        </tr>
-                
-                        
-                    </tbody>
-                </table>
-
+          <div className="input-container">
+            <div className="input-block">
+              <label>Produto</label>
+              <input
+                type="text"
+                value={nomeProduto}
+                onChange={(e) => setNomeProduto(e.target.value)}
+              />
             </div>
 
-            <button className="confirm-button" type="submit">Finalizar Pedido</button>
+            <div className="input-block">
+              <label>Quantidade</label>
+              <input
+                type="number"
+                value={qtd}
+                onChange={(e) => setQtd(e.target.value)}
+              />
+            </div>
 
+            <div className="input-block">
+              <label>Preço</label>
+              <input
+                type="number"
+                value={preco}
+                onChange={(e) => setPreco(e.target.value)}
+              />
+            </div>
+
+            <div className="add-button">
+              <button type="button" onClick={addNewProduct}>
+                <FaPlus size={25} color="#000" /> Adicionar
+              </button>
+            </div>
+
+            <table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Quantidade</th>
+                  <th>Preço</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {produtos.map((produto) => {
+                  return (
+                    <tr key={produto.nome}>
+                      <td>
+                        <span>{produto.nome}</span>
+                      </td>
+
+                      <td>
+                        <span>{produto.qtd}</span>
+                      </td>
+
+                      <td>
+                        <span>{formatPrice(Number(produto.preco))}</span>
+                      </td>
+
+                      <td>
+                        <button
+                          type="button"
+                          style={{ border: 0 }}
+                          onClick={() => deleteProduct(produto.nome)}
+                        >
+                          <AiFillDelete size={26} color="#000" />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+
+          <button className="confirm-button" type="submit">
+            Finalizar Pedido
+          </button>
         </form>
+      </div>
+    </>
+  );
+}
 
-        
-
-       </div>
-       </>
-    )
-  }
-  
-  export default Vendas;
-  
+export default Vendas;
