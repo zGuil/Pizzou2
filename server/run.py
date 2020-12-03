22acf87e83cfdd444aa04e2ec9679d05a77e9ec6
zGuil <guilherme.faraujo@aluno.faculdadeimpacta.com.br>
2020-12-02 21:13:25 -0300

    seila lá

--- server/run.py
+++ server/run.py
@@ -6,6 +6,6 @@ import sys
 config = app_config[app_active]
 config.APP = create_app(app_active)
 
-if __name__ == "__main__":
+def runner():
     config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
     reload(sys)
